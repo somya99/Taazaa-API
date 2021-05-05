59a2e6f56f0494672a7c9d455f7213a4fc7a5023
somya99 <dassomya1999@gmail.com>
2021-05-05 12:58:46 +0530

    initial commit

---
+++ DotNet/productapiAsync/Infrastructure/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using productapiAsync.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productapiAsync.Infrastructure
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+    }
+}
